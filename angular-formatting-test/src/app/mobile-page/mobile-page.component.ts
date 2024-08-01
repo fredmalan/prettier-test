92120bedd9b9e11810bedfe5610d79c59f6fa255
fredmalan <120031274+fredmalan@users.noreply.github.com>
2024-08-01 12:26:47 +0000

    update

---
+++ angular-formatting-test/src/app/mobile-page/mobile-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './mobile-page.component.html',
+  styleUrl: './mobile-page.component.scss'
+})
+export class MobilePageComponent {
+
+}
